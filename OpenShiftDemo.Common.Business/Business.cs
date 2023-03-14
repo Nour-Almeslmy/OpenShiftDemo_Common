d161c3f2e42274a89cb90ae67d50966ab24401ed
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-03-14 13:10:13 +0200

    git init

---
+++ OpenShiftDemo.Common.Business/Business.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenShiftDemo.Common.Business
+{
+    public class Business
+    {
+        public string Value { get; set; }
+
+        public Business()
+        {
+            this.Value = "Common Business";
+        }
+    }
+}
